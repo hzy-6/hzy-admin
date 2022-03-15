cc2e49b6fd9ba298f2027ae62c1e664082daf3a4
hzy <1396510655@qq.com>
2022-03-15 20:56:32 +0800

    🎉 优化图标组件

--- ui/src/components/AppIcons.vue
+++ ui/src/components/AppIcons.vue
@@ -1,33 +1,17 @@
 <template>
-  <component :is="currentIcon" v-if="!!name && currentIcon" />
+  <component :is="$props.name" v-if="!!name" />
 </template>
 
 <script>
-import { defineComponent, watch, ref } from "vue";
-import { getAllAntdIcons, getAllAntdIconNames } from "@/scripts/antd-icons";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "AppIconsCom",
   props: {
     name: String,
   },
-  setup(props) {
-    var antdIcons = getAllAntdIcons();
-    var names = getAllAntdIconNames();
-    //验证一下，组件名称是否存在合理的 名称集合里面
-    var currentIcon = ref(null);
-    currentIcon.value = names.findIndex((w) => w == props.name) > -1 ? antdIcons[props.name] : null;
-
-    watch(
-      () => props.name,
-      (value) => {
-        currentIcon.value = names.findIndex((w) => w == value) > -1 ? antdIcons[value] : null;
-      }
-    );
-
-    return {
-      currentIcon,
-    };
+  setup() {
+    return {};
   },
 });
 </script>
