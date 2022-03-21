8b3194c3c4e8a31c8c8e476a66b02a963a93fb39
hzy6 <1396510655@qq.com>
2022-03-21 14:34:17 +0800

    🎉 前端ui打包

--- src/HZY.WebHost/wwwroot/client/static/js/Redirect-bf521214.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-7aa787f9.js";import{b as r,m as c,s as n,o as p}from"./vendor-621951b9.js";const _={class:"p-15"},m={name:"RedirectCom"},h=Object.assign(m,{setup(u){const{params:t,query:o}=e.currentRoute.value,{path:s}=t;return e.replace({path:"/"+s,query:o}),(i,l)=>{const a=r("a-spin");return p(),c("div",_,[n(a)])}}});export{h as default};
---
+++ ui/dist/static/js/personal_center_service-fb596ca0.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-ea35a67a.js";const s="admin/PersonalCenter";var o={changePassword(r){return e(`${s}/changePassword`,r)},saveForm(r){return e(`${s}/saveForm`,r)}};export{o as s};
