a5b85ce705623fc7e03c55842ad4f0aee47e4901
hzy <1396510655@qq.com>
2022-04-04 17:34:14 +0800

    🎉 更新ui

--- src/HZY.WebHost/wwwroot/client/static/js/Redirect-eb8a1f7b.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-773034f6.js";import{f as r,v as c,y as n,q as p}from"./vendor-e651fafa.js";const _={class:"p-15"},m={name:"RedirectCom"},h=Object.assign(m,{setup(u){const{params:t,query:o}=e.currentRoute.value,{path:s}=t;return e.replace({path:"/"+s,query:o}),(i,l)=>{const a=r("a-spin");return p(),c("div",_,[n(a)])}}});export{h as default};
