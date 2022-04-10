375b4335488afac52ad614b32d4f764dc116de25
hzy <1396510655@qq.com>
2022-04-10 14:31:43 +0800

    🎉 发布前端文件

---
+++ src/HZY.WebHost/wwwroot/client/static/js/monitor_ef_core_service-0ae3f457.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-13b87e1a.js";const t="admin/MonitorEFCore";var o={getEFCoreMonitorContext(){return e(`${t}/GetEFCoreMonitorContext/`,null,!1)},getTimeConsumingRanking(){return e(`${t}/GetTimeConsumingRanking/`,null,!1)},getNewest(){return e(`${t}/GetNewest/`,null,!1)}};export{o as s};
