05b4b2995b7d53245b24af9e436f3b2563c8c54f
HZY <1396510655@qq.com>
2022-04-17 09:51:29 +0000

    add ui/src/views/base/member/components/Details.vue.

---
+++ ui/src/views/base/member/components/Details.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是从会员管理跳转过来的：<br />
+    带过来的id参数是：{{ id }}<br />
+    带过来的title参数是：{{ title }}<br />
+  </div>
+</template>
+
+<script>
+export default { name: "base_member_info_Details" };
+</script>
+
+<script setup>
+import router from "@/router/index";
+const { params } = router.currentRoute.value;
+const { id, title } = params;
+</script>
+
+<style lang="less" scoped></style>
