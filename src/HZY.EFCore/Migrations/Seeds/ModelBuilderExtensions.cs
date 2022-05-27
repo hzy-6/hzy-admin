8e81fd694055dde569d5615078fb1e6d007bf0de
hzy <1396510655@qq.com>
2022-05-27 21:17:17 +0800

    🎉

--- src/HZY.EFCore/Migrations/Seeds/ModelBuilderExtensions.cs
+++ src/HZY.EFCore/Migrations/Seeds/ModelBuilderExtensions.cs
@@ -15,14 +15,19 @@ namespace HZY.EFCore.Migrations.Seeds
         /// <param name="modelBuilder"></param>
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            MigrationsLowCodeTableModelBuilderExtensions.Seed(modelBuilder); MigrationsLowCodeTableInfoModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsLowCodeTableModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsLowCodeTableInfoModelBuilderExtensions.Seed(modelBuilder);
             MigrationsMemberModelBuilderExtensions.Seed(modelBuilder);
-            MigrationsSysDataAuthorityModelBuilderExtensions.Seed(modelBuilder); MigrationsSysDictionaryModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysDataAuthorityModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysDictionaryModelBuilderExtensions.Seed(modelBuilder);
             MigrationsSysFunctionModelBuilderExtensions.Seed(modelBuilder);
             MigrationsSysMenuModelBuilderExtensions.Seed(modelBuilder);
-            MigrationsSysMenuFunctionModelBuilderExtensions.Seed(modelBuilder); MigrationsSysOrganizationModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysMenuFunctionModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysOrganizationModelBuilderExtensions.Seed(modelBuilder);
             MigrationsSysPostModelBuilderExtensions.Seed(modelBuilder);
-            MigrationsSysRoleModelBuilderExtensions.Seed(modelBuilder); MigrationsSysRoleMenuFunctionModelBuilderExtensions.Seed(modelBuilder); MigrationsSysUserModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysRoleModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysRoleMenuFunctionModelBuilderExtensions.Seed(modelBuilder);
+            MigrationsSysUserModelBuilderExtensions.Seed(modelBuilder);
             MigrationsSysUserPostModelBuilderExtensions.Seed(modelBuilder);
             MigrationsSysUserRoleModelBuilderExtensions.Seed(modelBuilder);
         }
