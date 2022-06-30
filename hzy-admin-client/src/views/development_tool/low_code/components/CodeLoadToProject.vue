7aa9465ad1c1246e977e71ee334340f3ea0d1793
hzy6 <1396510655@qq.com>
2022-06-30 17:07:11 +0800

    🎉 优化代码生成界面

---
+++ hzy-admin-client/src/views/development_tool/low_code/components/CodeLoadToProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-form ref="formRef" layout="vertical" :model="state.vm.form">
+    <a-row :gutter="[15, 15]">
+      <a-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+        <a-form-item label="真实姓名" ref="name" name="name">
+          <a-input v-model:value="state.vm.form.name" placeholder="请输入" />
+        </a-form-item>
+      </a-col>
+    </a-row>
+  </a-form>
+</template>
+<script setup>
+import { reactive } from "vue";
+
+//定义组件事件
+// const emits = defineEmits(["onSuccess"]);
+const props = defineProps({
+  tableName: String,
+});
+
+const state = reactive({
+  vm: {
+    form: {},
+  },
+});
+</script>
