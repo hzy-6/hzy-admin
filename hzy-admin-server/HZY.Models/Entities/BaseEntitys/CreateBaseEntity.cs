b3aeeab5b214630021a914462cfd412695b18493
HZY <1396510655@qq.com>
2023-01-11 09:07:35 +0000

    update hzy-admin-server/HZY.Models/Entities/BaseEntitys/CreateBaseEntity.cs.
    Signed-off-by: HZY <1396510655@qq.com>

--- hzy-admin-server/HZY.Models/Entities/BaseEntitys/CreateBaseEntity.cs
+++ hzy-admin-server/HZY.Models/Entities/BaseEntitys/CreateBaseEntity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace HZY.Models.Entities.BaseEntitys;
 public class CreateBaseEntity<TKey> : BaseEntity<TKey>, ICreateBaseEntity, IBaseEntity<TKey>
 {
     public virtual Guid? CreatorUserId { get; set; }
-    public virtual DateTime CreationTime { get; set; }
+    public virtual DateTime CreationTime { get; set; } = DateTime.Now;
 }
 
 public interface ICreateBaseEntity
